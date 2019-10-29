171bf98c7d36206e715db4b72577d6e2db217119
SamuelZeru <samuel.fzeru@gmail.com>
2019-10-29 10:27:04 -0400

    code format

--- src/leetCode/TwoSum.java
+++ src/leetCode/TwoSum.java
@@ -9,10 +9,10 @@ public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target) {
         int[] twoSum = new int[2];
-        for(int i=0; i<nums.length; i++){
-            int num =  target - nums[i];
-            for(int j = i+1; j < nums.length; j++){
-                if(num == nums[j]){
+        for (int i = 0; i < nums.length; i++) {
+            int num = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (num == nums[j]) {
                     twoSum[0] = i;
                     twoSum[1] = j;
                     break;
@@ -24,7 +24,7 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
-        int[] input = {3,2,4};
+        int[] input = {3, 2, 4};
         int target = 6;
         System.out.println(Arrays.toString(twoSum(input, target)));
     }
