880c38a0310f3cea89880476be4ead426f52e0e8
SamuelZeru <samuel.fzeru@gmail.com>
2019-11-05 00:00:00 -0500

    update bubble sort

--- src/BubbleSort/BubbleSortMain.java
+++ src/BubbleSort/BubbleSortMain.java
@@ -7,16 +7,16 @@ import java.util.Arrays;
  */
 public class BubbleSortMain {
 
-    public static void main(String[] args){
-        int arr[] = {64,25,12,22,11};
+    public static void main(String[] args) {
+        int arr[] = {64, 25, 12, 22, 11};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSort(int[] array){
-        for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array.length - i - 1; j++){
-                if(array[j] > array[j + 1]){
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
